f91f07a67df08d1c59eb5ddcfacb787b10c348d7
Des Penny <despenny@gmail.com>
2014-04-14 19:02:58 +1000

    Solution for Bodacious Udacity Problem

--- Lesson_1_Problem_Set/06-Bodacious_Udacity/hints.py
+++ Lesson_1_Problem_Set/06-Bodacious_Udacity/hints.py
@@ -3,4 +3,4 @@
 # From the README:
 # We do NOT accept pull requests that have deleted another contributer's hint or solution without a very clear reason
 # ONLY use concepts covered in the class so far
-# Do NOT give solutions here - ONLY hints
+# Do NOT give solutions here - ONLY hints.
